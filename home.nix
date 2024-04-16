e245497b11976bccc8e699973c8ae9c300b18a14
Evgeny Zemtsov <eugene.zemtsov@gmail.com>
2024-04-16 18:03:05 +0200

    remove custom firefox config
    something is broken with webgl in firefox anyways, forced to use chromium

--- home.nix
+++ home.nix
@@ -34,33 +34,8 @@
   # global nixpkgs set.
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = false;
-
   home-manager.users.ezemtsov = {
     home.stateVersion = config.system.stateVersion;
-
-    programs.firefox = {
-      enable = true;
-      profiles.default = {
-        id = 0;
-        name = "default";
-        extraConfig = ''
-          user_pref("ui.key.menuAccessKeyFocuses", false);
-          user_pref("browser.link.open_newwindow", 1);
-          user_pref("browser.tabs.inTitlebar", 0);
-          user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-        '';
-        userChrome = ''
-          /* Hide Tab bar with only one Tab - [110] */
-          #tabbrowser-tabs .tabbrowser-tab:only-of-type,
-          #tabbrowser-tabs .tabbrowser-tab:only-of-type + #tabbrowser-arrowscrollbox-periphery{
-            display:none !important;
-          }
-          #tabbrowser-tabs, #tabbrowser-arrowscrollbox {min-height:0!important;}
-          /* #TabsToolbar:not(:hover) */ #alltabs-button {display:none !important;}
-        '';
-      };
-    };
-
     services.flameshot.enable = true;
     services.dunst.enable = true;
   };
