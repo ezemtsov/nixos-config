e49071b0dabc30f8b8a76fa70e529fb92538e82e
Evgeny Zemtsov <eugene.zemtsov@gmail.com>
2024-12-04 13:48:24 +0100

    enable thermald

--- configuration.nix
+++ configuration.nix
@@ -69,6 +69,9 @@ in
   # Enable power saving
   powerManagement.enable = true;
 
+  # Adjust performance automatically
+  services.thermald.enable = true;
+
   # Emacs
   services.emacs = {
     enable = false;
