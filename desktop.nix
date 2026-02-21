96cf1857ccff228418e409d49976451a98139987
Evgeny Zemtsov <eugene.zemtsov@gmail.com>
2026-02-21 13:48:43 +0100

    [env] add .net 10 to DOTNET_ROOT

--- desktop.nix
+++ desktop.nix
@@ -10,6 +10,7 @@
       DOTNET_ROOT = with pkgs.dotnetCorePackages; combinePackages [
         sdk_8_0
         sdk_9_0
+        sdk_10_0
       ] + "/share/dotnet";
 
       EDITOR = "emacsclient";
